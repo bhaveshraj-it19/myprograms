547571da5a3d4f2473928f5d48581295a78fe507
bhaveshraj-it19 <55017924+bhaveshraj-it19@users.noreply.github.com>
2019-11-18 17:39:28 +0530

    first commit

---
+++ prog 67.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the two numbers to compare :");
+scanf("%d %d",&a,&b);
+if(a>b)
+{
+printf("%d is the greatest number",a);
+}
+else
+{
+printf("%d is the greatest number",b);
+}
+return 0;
+}
